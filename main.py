aee7c296c0bfe9f4ff7df458bbd085ee21c746f1
ZackBystrom10 <835337@mystma.org>
2022-09-21 08:54:58 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#WRITE YOUR CODE IN THIS FILE
+from re import X
+
+
+def close10(x,y):
+    if x>y:
+        return x
+    elif x<y:
+        return y
+    else:
+        return 0
+print(close10(4,2))
